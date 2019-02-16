f9355e95b2fa004d1aa623b0bf1dd1109ffb7be2
Jordan Mackie <jm354@st-andrews.ac.uk>
2019-02-16 19:45:56 +0000

    Add plots of input variables distributions

--- code/__main__.py
+++ code/__main__.py
@@ -4,11 +4,26 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def load_inputs_and_outputs(filename):
-    data = np.loadtxt(filename, delimiter=",", skiprows=1)
+    data = np.genfromtxt(filename, delimiter=",", skip_header=True, invalid_raise=True)
     inputs = data[:, 0:8]
     outputs = data[:, 8:]
     return inputs, outputs
 
 
+def plot_distributions(x):
+    n_cols = x.shape[1]
+    for col_index in range(n_cols):
+        fig = plt.figure(col_index)
+        col_values = x[:, col_index]
+        plt.hist(col_values)
+        plt.title("Distribution of variable X{}".format(col_index + 1))
+        plt.ylabel("Frequency")
+        plt.xlabel("Value")
+        plt.show()
+
+
 x, y = load_inputs_and_outputs("data/ENB2012_data.csv")
+plot_distributions(x)
+
+# Add column of ones
 x = np.c_[np.ones_like(x), x]
