b48fb7364b64c2bb03351fcd36f7374b7160f12f
Jordan Mackie <jm354@st-andrews.ac.uk>
2019-02-16 17:45:51 +0000

    Load data and seperate inputs and outputs

--- code/__main__.py
+++ code/__main__.py
@@ -2,3 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
+def load_inputs_and_outputs(filename):
+    data = np.loadtxt(filename, delimiter=",", skiprows=1)
+    inputs = data[:, 0:8]
+    outputs = data[:, 8:]
+    return inputs, outputs
+
+
+x, y = load_inputs_and_outputs("data/ENB2012_data.csv")
+x = np.c_[np.ones_like(x), x]
