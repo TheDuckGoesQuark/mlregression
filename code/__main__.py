bfb2e774a3e093cb81d1738fc1a313596346d08d
Jordan Mackie <jmackie97@hotmail.com>
2019-02-22 22:28:15 +0000

    Update report to justify removing X6, and remove X6 column in code

--- code/__main__.py
+++ code/__main__.py
@@ -3,7 +3,9 @@ import numpy as np
 
 x, y = featureselection.load_inputs_and_outputs("data/ENB2012_data.csv")
 # featureselection.visualise(x, y)
-featureselection.spearman(x, y)
+# featureselection.spearman(x, y)
 
-# Add column of ones
-x = np.c_[np.ones_like(x), x]
+# Remove X6 from features as discussed in report
+x = np.delete(x, 5, 1)
+print(x)
+
