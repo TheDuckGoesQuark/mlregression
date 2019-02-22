da7a067e4ce9f209f4aa3e9e8494bca1a071b32f
Jordan Mackie <jmackie97@hotmail.com>
2019-02-22 22:31:12 +0000

    Remove X8 and justify in report

--- code/__main__.py
+++ code/__main__.py
@@ -5,7 +5,8 @@ x, y = featureselection.load_inputs_and_outputs("data/ENB2012_data.csv")
 # featureselection.visualise(x, y)
 # featureselection.spearman(x, y)
 
-# Remove X6 from features as discussed in report
+# Remove X6 and X8 from features as discussed in report
 x = np.delete(x, 5, 1)
+x = np.delete(x, 6, 1)
 print(x)
 
